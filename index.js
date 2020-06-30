1a08c50fcb4b1677ad443654b37d3a74d9db6cd5
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-06-30 09:54:41 +0300

    Add pop and shift task

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 'use strict';
 
-const a = [1, 2, 3];
-const b = [4, 5, 6];
+const a = ['js', 'css', 'jq'];
 
-a.push(4,5,6);
-
-alert(a);
-
-b.unshift(4,5,6);
-alert(b);
+alert(a.shift());
+alert(a.pop());
